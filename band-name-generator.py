a6002ca8c02dd320bb69f5856394f8d39e202ee4
Manny <immanuelmonroe@gmail.com>
2023-03-13 21:31:40 -0400

    Added all code from day 1

---
+++ band-name-generator.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print('Welcome to the super-mega-awesome-totally-original-band-name-generator!')
+#2. Ask the user for the city that they grew up in.
+city = input("What's city did you grow up in?\n")
+#3. Ask the user for the name of a pet.
+petName = input("What's the name of your pet? Or your imaginary one?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Hmm... your band name should totally be " + city + " " + petName + "!")
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
---
+++ day-1-variables-start.py
@@ -0,0 +1,19 @@
+# name = input("What is your name?")
+# print(name)
+
+# # Both names will be printed rather than one variable overriding the other
+
+# name = "Jack"
+# print(name)
+
+# name = "Angela"
+# print(name)
+
+# original answer to finding length of name - print(len("What is your name?"))
+# Alternate version of finding name length using variables
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+# Try to set variable names that are easy to read rather than a single letter\
